c6398fee2b3abb345a10fb917be733beb4f0b80b
marius talpos <mariustalpos@mariuss-MacBook-Pro.local>
2016-04-19 15:24:49 -0400

    initial commit

--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-var CompsModule = angular.module('CompsApp', []);
-
-CompsModule.controller('ZillowController', ['$scope', ZillowController]);
-
-function ZillowController($scope){
-	window.$scope = $scope;
-}
