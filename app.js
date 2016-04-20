dd8101854ccd70ba9e49fc64929ea4c8f0b925ae
marius talpos <mariustalpos@mariuss-MacBook-Pro.local>
2016-04-19 21:06:29 -0400

    browserify fix

--- app.js
+++ app.js
@@ -14,12 +14,19 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(methodOverride('X-HTTP-Method-Override')); 								//Simulate DELETE/PUT requests
 
+app.use(express.static('public'));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+app.route('/')
+	.get(function(req, res){
+		res.sendFile(__dirname + '/public/index.html');
+	})
+
 app.listen(port, function () {
-  console.log('Example app listening on port 5000!');
+  console.log('Example app listening on port ' + port);
 });
